b12974ae04d7b128e627bf76f1401a4124c1264c
박명규 <audrb0999@naver.com>
2026-02-20 17:29:04 +0900

    [빈 스코프] 프로토타입 스코프 - ObjectFactory, ObjectProvider

--- src/test/java/com/hello/scope/SingletonWithPrototypeTest.java
+++ src/test/java/com/hello/scope/SingletonWithPrototypeTest.java
@@ -1,8 +1,10 @@
 package com.hello.scope;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -60,7 +62,7 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         // PrototypeBean이 스프링 컨테이너 생성 시점에 의존관계 자동 주입되어 같은 참조값을 사용한다
         // 따라서 스프링 컨테이너가 요청할때마다 생성하지 않는다
         // ※ 여기서 의문...? 그럼 Scope prototype을 쓰는 의미가...? 없는데???
@@ -68,13 +70,20 @@ public class SingletonWithPrototypeTest {
 
 
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
+//
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        // private Provider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            // PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
