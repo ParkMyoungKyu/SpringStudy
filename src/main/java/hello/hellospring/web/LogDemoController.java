5ce5f3e3567704bbba4960cbd59aec10b6a2338a
박명규 <audrb0999@naver.com>
2026-02-23 10:32:36 +0900

    [빈 스코프] request 스코프 예제 만들기

---
+++ src/main/java/hello/hellospring/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package hello.hellospring.web;
+
+import hello.hellospring.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        System.out.println("LogDemoController.logDemo");
+        String requestUrl = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestUrl);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
